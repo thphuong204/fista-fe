e3910839ef120acf015726aff19db576311a4867
Phuong Tran <thphuong204@gmail.com>
2022-12-21 11:44:17 +0700

    Initialize Material UI ThemeProvider

---
+++ src/contexts/ThemeProvider.js
@@ -0,0 +1,50 @@
+import { CssBaseline } from "@mui/material";
+import {
+  createTheme,
+  ThemeProvider as MUIThemeProvider,
+} from "@mui/material/styles";
+
+const PRIMARY = {
+  lighter: "#fffefd",
+  light: "#fffdfa",
+  main: "#fffcf5",
+  dark: "#fffbf2",
+  darker: "#FFFAF0"
+};
+const SECONDARY = {
+  lighter: "#ffffcc",
+  light: "#fff2b2",
+  main: "#ffde8a",
+  dark: "#ffd475",
+  darker: "#ffcc66"
+};
+const SUCCESS = {
+  lighter: "#E9FCD4",
+  light: "#AAF27F",
+  main: "#54D62C",
+  dark: "#229A16",
+  darker: "#08660D",
+  contrastText: "#FFF",
+};
+
+function ThemeProvider({ children }) {
+  const themeOptions = {
+    palette: {
+      primary: PRIMARY,
+      secondary: SECONDARY,
+      success: SUCCESS,
+    },
+    shape: { borderRadius: 8 },
+  };
+
+  const theme = createTheme(themeOptions);
+
+  return (
+    <MUIThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </MUIThemeProvider>
+  );
+}
+
+export default ThemeProvider;

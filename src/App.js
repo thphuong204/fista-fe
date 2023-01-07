745db459ef9c6279af9fa2348cbc6c50a368c39e
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2023-01-07 11:48:48 +0700

    Initialize App.css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import Router from "./routes";
 import { AuthProvider } from "./contexts/AuthContext";
 import ThemeProvider from "./contexts/ThemeProvider";
+import './App.css';
 
 function App() {
   return (

8cc66b75164c60c3d4f8fec38f34d5f23fb69fc3
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2023-01-12 21:25:38 +0700

    Remove console.log line

--- src/components/form/FTextField.js
+++ src/components/form/FTextField.js
@@ -3,7 +3,6 @@ import { TextField } from "@mui/material";
 
 function FTextField({ name, ...other }) {
   const {control}  = useFormContext();
-  console.log(useFormContext());
 
   return (
     <Controller

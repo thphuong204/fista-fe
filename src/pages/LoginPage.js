a60a997417b27ae26270d7e2d14af8576751dcd4
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2023-01-12 21:22:12 +0700

    Improve for LoginPage

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,36 +1,55 @@
-import { Button, Stack, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import { 
+  Button, 
+  Stack, 
+  Alert,
+  Typography,
+  InputAdornment,
+  IconButton
+} from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import { useForm, FormProvider } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
 import { FTextField } from "../components/form";
 
 const LoginSchema = Yup.object().shape({
-  username: Yup.string().required("Username is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string().required("Password is required"),
 });
 const defaultValues = {
-  username: "",
+  email: "",
+  password: "",
 };
 
 function LoginPage() {
   let navigate = useNavigate();
   let location = useLocation();
   let auth = useAuth();
+  const [showPassword, setShowPassword] = useState(false);
 
   const methods = useForm({
     resolver: yupResolver(LoginSchema),
     defaultValues,
   });
 
+  const {
+    handleSubmit,
+    reset,
+    setError,
+    formState: { errors, isSubmitting },
+  } = methods;
+
   const onSubmit = async (data) => {
     console.log("data", data);
 
     let from = location.state?.from?.pathname || "/";
-    let username = data.username;
-    console.log("from", from);
-    auth.login(username, () => {
+    let { email, password } = data;
+
+    auth.login( { email, password }, () => {
       navigate(from, { replace: true });
     });
   };
@@ -40,11 +59,31 @@ function LoginPage() {
         <form onSubmit={methods.handleSubmit(onSubmit)}>
 
           <Stack spacing={3} sx={{ minWidth: "350px" }}>
+          {!!errors.responseError && (
+            <Alert severity="error">{errors.responseError.message}</Alert>
+          )}
             <Typography variant="h4" textAlign="center">
               Login
             </Typography>
 
-            <FTextField name="username" label="Username" />
+            <FTextField name="email" label="Email" />
+            <FTextField
+            name="password"
+            label="Password"
+            type={showPassword ? "text" : "password"}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={() => setShowPassword(!showPassword)}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
 
             <Button type="submit" variant="contained">
               Login

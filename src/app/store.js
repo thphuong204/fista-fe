0f64ba740349f12c992614e0b6d062a250405d53
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2023-01-13 19:31:47 +0700

    Combine walletReducer in store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
+import walletReducer from "../features/wallet/walletSlice";
 import transactionReducer from "../features/transaction/transactionSlice";
 
 const rootReducer = combineReducers({
   user: userReducer,
-  transaction: transactionReducer
+  transaction: transactionReducer,
+  wallet: walletReducer
 });
 
 const store = configureStore({

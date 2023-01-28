0df977b1391ef395fcc9c3e1469908a45d51178a
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2023-01-28 18:23:25 +0700

    Initialize CustomizeButton

---
+++ src/components/CustomizedButton.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button } from "@mui/material";
+
+function SmallButton({ text}) {
+
+  return (
+    <Button 
+      size="small" 
+      style={{ 
+        margin: "0 10px",
+        backgroundColor: "#ffde8a",
+        color: "#000",
+        boxShadow: "1px 1px 10px 1px rgba( 176, 176, 176, 0.87 ), -1px -1px 10px 1px rgba( 176, 176, 176, 0.87 )"
+      }}
+    >
+      {text}
+    </Button>
+  )
+}
+
+export  { SmallButton };

8ff89c5e0d4a6386207a05cf7f0d9f30afe7e192
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2023-02-05 10:29:22 +0700

    CSS text-transform

--- src/features/category/CategoriesList.js
+++ src/features/category/CategoriesList.js
@@ -38,10 +38,10 @@ function CategoriesList({categoryById, currentPageCategories, totalCategories}) 
                       key={index}
                       sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                     >
-                      <TableCell component="th" scope="row">
+                      <TableCell scope="row" style={{textTransform: "capitalize"}}>
                         {categoryById[item]?.name}
                       </TableCell>
-                      <TableCell>
+                      <TableCell style={{textTransform: "capitalize"}}>
                         {categoryById[item]?.classification}
                       </TableCell>
                     </TableRow>
